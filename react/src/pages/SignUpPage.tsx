26c9f5aaa96815afb4c1dc07a7aea10ac7d55940
Jeffrey Fulmer Gardner <JeffreyFulmerGardner@lbl.gov>
2024-09-11 11:03:53 -0700

    Added Signup explanation and diagram

--- react/src/pages/SignUpPage.tsx
+++ react/src/pages/SignUpPage.tsx
@@ -3,28 +3,19 @@ import useFetch from "../hooks/useFetch";
 import BodyComponent from "../components/BodyComponent";
 import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
 import useSignUp from "../hooks/useSignUp";
-
+import Image from "react-bootstrap/Image";
 import { Dispatch, SetStateAction } from "react";
 import userInterface from "../interfaces/userInterface";
 
 // https://developers.google.com/identity/gsi/web/reference/js-reference
 
-const SignUpPage = (properties: {
-  isLoggedIn: boolean;
-  setStateUser: Dispatch<SetStateAction<userInterface>>;
-}) => {
-  const { error, loading, handleGoogle } = useFetch(
-    "https://JeffreyFG.net/api/auth/login",
-    properties.setStateUser
-  );
+const SignUpPage = (properties: { isLoggedIn: boolean; setStateUser: Dispatch<SetStateAction<userInterface>> }) => {
+  const { error, loading, handleGoogle } = useFetch("https://JeffreyFG.net/api/auth/login", properties.setStateUser);
   const { isSignUpPermitted, getPermissionToSignUp } = useSignUp();
   getPermissionToSignUp();
 
   return (
-    <BodyComponent
-      isloggedIn={properties.isLoggedIn}
-      setStateUser={properties.setStateUser}
-    >
+    <BodyComponent isloggedIn={properties.isLoggedIn} setStateUser={properties.setStateUser}>
       <main
         style={{
           display: "flex",
@@ -53,7 +44,14 @@ const SignUpPage = (properties: {
                 />
               </>
             ) : (
-              <p>Sign ups are not permitted anymore</p>
+              <>
+                <p>Sign ups are not permitted anymore</p>
+                <br />
+                <p>The sign up functionality is no longer permitted as it was created to show that I understand how to create a complete auth flow</p>
+                <br></br>
+                <p> In an effort to demonstrate this further I have created a full diagram for this web application</p>
+                <Image fluid src="/images/JeffreyFGServerStructure.drawio.svg" rounded />
+              </>
             )}
           </>
         )}
